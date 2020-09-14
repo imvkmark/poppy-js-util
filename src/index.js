f2190a8a82b6159271f40f0316f29127224a26bb
duoli <zhaody901@126.com>
2020-09-15 00:23:44 +0800

    export build Url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { browser as _browser } from './poppy/_http.js';
 
-export { isUrl } from './poppy/_http.js';
+export { isUrl, buildUrl } from './poppy/_http.js';
 
 export const name = 'base';
 export const browser = _browser();
