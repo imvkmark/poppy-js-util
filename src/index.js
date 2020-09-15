050bb10ec88def82160f86236efc69d4375c9dee
duoli <zhaody901@126.com>
2020-09-15 11:50:43 +0800

    feat(application) : add store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { browser as _browser } from './poppy/_http.js';
-import { viewport as _viewport } from './poppy/_view.js';
-import { windowHeight as _windowHeight } from './poppy/_view.js';
+import { viewport as _viewport, windowHeight as _windowHeight } from './poppy/_view.js';
 
 export { isUrl, buildUrl, domain, queryString } from './poppy/_http.js';
 export { classie } from './poppy/_dom.js';
 export { toJson } from './poppy/_json.js';
+export { localStore, sessionStore } from './poppy/_application.js';
 export { isEmail, isMobile, isTouchDevice, random, uniqueId, debugTime, isNumber, removerBlank } from './poppy/_util.js';
 export { fullScreen, exitFullScreen } from './poppy/_view.js';
 
---
+++ src/poppy/_application.js
@@ -0,0 +1,96 @@
+/**
+ * 实现localStorage缓存的 存, 取, 删操作
+ * @param key 对象, 批量设置
+ * @param val 有值:设置; 无值: 获取; null, 删除;
+ * @param {function} decoration
+ */
+import { forEach } from 'lodash-es';
+
+export const localStore = (key, val, decoration = function(key) {
+    return key;
+}) => {
+    /**
+     * localStorage内存溢出时，则清空后继续保存
+     * @param {string} key 缓存key
+     * @param {string} data JSON.stringify后的数据
+     */
+    let _localStorageOverflow = (key, data) => {
+        try {
+            localStorage.setItem(key, data);
+        } catch (e) { // 当缓存溢出，则清空后继续保存
+            if (e.code === 'QUOTA_EXCEEDED_ERR_CODE') {
+                localStorage.removeItem(key);
+                localStorage.setItem(key, data);
+            } else {
+                console.log(e);
+            }
+        }
+    };
+
+    // 本地数据存储封装，没有过期时间限制，仅限于该页面的协议
+    if (val === null) {
+        if (typeof key === 'object') {
+            _.each(key, function(ele, idx) {
+                _localStorageOverflow(decoration(idx), ele); // 存储数据
+            });
+            return;
+        } else {
+            localStorage.removeItem(decoration(key));// 移除数据
+            return;
+        }
+    }
+    if (typeof val === 'undefined') {
+        let data = localStorage.getItem(decoration(key));
+        if (typeof data === 'string') {
+            try {
+                data = JSON.parse(data);
+                return data; // 获取数据
+            } catch (err) {
+                return localStorage.getItem(decoration(key)); // 获取数据
+            }
+        }
+        return data; // 获取数据
+    }
+    if (typeof val === 'object') {
+        _localStorageOverflow(decoration(key), JSON.stringify(val));
+    } else {
+        _localStorageOverflow(decoration(key), val);
+    }
+};
+
+
+/**
+ * 实现sessionStorage缓存的 存, 取, 删操作
+ * @param key 对象, 批量设置
+ * @param val 有值:设置; 无值: 获取; null, 删除;
+ */
+export const sessionStore = (key, val) => {  // 本地数据存储封装，随页面回话结束而结束，仅限于该页面的协议
+    if (val === null) {
+        if (typeof key === 'object') {
+            forEach(key, function(ele, idx) {
+                sessionStorage.setItem(idx, ele);
+            });
+            return;
+        } else {
+            sessionStorage.removeItem(key);
+            return;
+        }
+    }
+    if (typeof val === 'undefined') {
+        let data = sessionStorage.getItem(key);
+        if (typeof data === 'string') {
+            try {
+                data = JSON.parse(data);
+                return data; // 获取数据
+            } catch (err) {
+                return sessionStorage.getItem(key); // 获取数据
+            }
+        }
+        return data; // 获取数据
+    }
+    if (typeof val === 'object') {
+        sessionStorage.setItem(key, JSON.stringify(val));
+    } else {
+        sessionStorage.setItem(key, val);
+    }
+};
